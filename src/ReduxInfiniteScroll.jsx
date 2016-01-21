74d2c2d8430d1091540c0e385070af5e81c88c7d
Chris Conley <pair+chrisconley@realscout.com>
2016-01-21 09:53:00 -0800

    Adds source code and test

---
+++ src/ReduxInfiniteScroll.jsx
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+function topPosition(domElt) {
+  if (!domElt) {
+    return 0;
+  }
+  return domElt.offsetTop + topPosition(domElt.offsetParent);
+}
+
+export default class ReduxInfiniteScroll extends Component {
+
+  constructor(props) {
+    super(props);
+    this.scrollFunction = this.scrollListener.bind(this);
+  }
+
+  componentDidMount () {
+    this.attachScrollListener();
+  }
+
+  componentDidUpdate () {
+    this.attachScrollListener();
+  }
+
+  _findElement() {
+    return this.props.elementIsScrollable ? ReactDOM.findDOMNode(this) : window;
+  }
+
+  attachScrollListener () {
+    if (!this.props.hasMore || this.props.loadingMore) return;
+    let el = this._findElement();
+    el.addEventListener('scroll', this.scrollFunction, true);
+    el.addEventListener('resize', this.scrollFunction, true);
+    this.scrollListener();
+  }
+
+  _elScrollListener() {
+    let el = ReactDOM.findDOMNode(this);
+    let topScrollPos = el.scrollTop;
+    let totalContainerHeight = el.scrollHeight;
+    let containerFixedHeight = el.offsetHeight;
+    let bottomScrollPos = topScrollPos + containerFixedHeight;
+
+    return (totalContainerHeight - bottomScrollPos);
+  }
+
+  _windowScrollListener() {
+    let el = ReactDOM.findDOMNode(this);
+    let windowScrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    let elTotalHeight = topPosition(el) + el.offsetHeight;
+    let currentBottomPosition = elTotalHeight - windowScrollTop - window.innerHeight;
+
+    return currentBottomPosition;
+  }
+
+  scrollListener() {
+    // This is to prevent the upcoming logic from toggling a load more before
+    // redux has passed any data to the component
+    if (this.props.items <= 0) return;
+
+    setTimeout(() => {
+      let bottomPosition = this.props.elementIsScrollable ? this._elScrollListener() : this._windowScrollListener();
+
+      if (bottomPosition < Number(this.props.threshold)) {
+        this.detachScrollListener();
+        this.props.loadMore();
+      }
+    }, 0);
+  }
+
+  detachScrollListener () {
+    let el = this._findElement();
+    el.removeEventListener('scroll', this.scrollFunction, true);
+    el.removeEventListener('resize', this.scrollFunction, true);
+  }
+
+  componentWillUnmount () {
+    this.detachScrollListener();
+  }
+
+  renderLoader() {
+    return (this.props.loadingMore && this.props.showLoader) ? this.props.loader : undefined;
+  }
+
+  render () {
+    return (
+        <div className="rs-infinite-scroll" style={{height: this.props.containerHeight}}>
+          {this.props.items.map((item, i) => {
+            return item;
+          })}
+          {this.renderLoader()}
+        </div>
+    )
+  }
+}
+
+ReduxInfiniteScroll.propTypes = {
+  elementIsScrollable: React.PropTypes.bool,
+  containerHeight: React.PropTypes.oneOfType([
+    React.PropTypes.number,
+    React.PropTypes.string
+  ]),
+  threshold: React.PropTypes.number,
+  hasMore: React.PropTypes.bool,
+  loadingMore: React.PropTypes.bool,
+  loader: React.PropTypes.any,
+  showLoader: React.PropTypes.bool,
+  loadMore: React.PropTypes.func.isRequired,
+  items: React.PropTypes.array.isRequired
+};
+
+ReduxInfiniteScroll.defaultProps = {
+  elementIsScrollable: true,
+  containerHeight: '100%',
+  threshold: 100,
+  hasMore: true,
+  loadingMore: false,
+  loader: <div style={{'text-align': 'center'}}>Loading...</div>,
+  showLoader: true
+};
---
+++ test/ReduxInfiniteScroll-test.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react/lib/ReactTestUtils';
+import ReduxInfiniteScroll from '../ReduxInfiniteScroll';
+
+describe('ReduxInfiniteScroll', () => {
+  let loadMore, items;
+
+  beforeEach(() => {
+    loadMore = () => {
+      items = items.concat(['item 4', 'item 5'])
+    };
+
+    items = [<div height="250px"></div>, <div height="250px"></div>, <div height="250px"></div>];
+  });
+
+  describe('default props', () => {
+    it('defaults to 100%', () => {
+      const reactElement = <ReduxInfiniteScroll loadMore={loadMore}
+                                             items={items}/>;
+      const render = TestUtils.renderIntoDocument(reactElement);
+      const node = ReactDOM.findDOMNode(render);
+      expect(node.style.height).toEqual('100%');
+    });
+  });
+
+  describe('containerHeight', () => {
+    it('defaults to 100%', () => {
+      const reactElement = <ReduxInfiniteScroll loadMore={loadMore}
+                                             items={items}/>;
+      const render = TestUtils.renderIntoDocument(reactElement);
+      const node = ReactDOM.findDOMNode(render);
+      expect(node.style.height).toEqual('100%');
+    });
+    it('should set the height of the div when given the correct props', () => {
+      const reactElement = <ReduxInfiniteScroll containerHeight="500px"
+                                             loadMore={loadMore}
+                                             items={items}/>;
+      const render = TestUtils.renderIntoDocument(reactElement);
+      const node = ReactDOM.findDOMNode(render);
+      expect(node.style.height).toEqual('500px');
+    });
+  });
+
+  xdescribe('threshold', () => {
+    it('defaults to setting the threshold to 100', () => {
+      const reactElement = <ReduxInfiniteScroll containerHeight="500px"
+                                             loadMore={loadMore}
+                                             items={items}/>;
+      const render = TestUtils.renderIntoDocument(reactElement);
+      const node = ReactDOM.findDOMNode(render);
+      //node.scrollTop = 499;
+      //reactElement.props.loadMore();
+      TestUtils.Simulate.scroll(node, {deltaY: 450});
+
+      expect(items).toEqual('')
+    });
+
+    it('actually adds the element to the page', () => {
+      const reactElement = <ReduxInfiniteScroll containerHeight="500px"
+                                             loadMore={loadMore}
+                                             items={items}/>;
+      //const render = TestUtils.renderIntoDocument(reactElement);
+
+
+      var parentDiv = document.createElement('div');
+      document.body.appendChild(parentDiv);
+      var component = ReactDOM.render(reactElement, parentDiv);
+      const node = ReactDOM.findDOMNode(component);
+      TestUtils.Simulate.scroll(node, {deltaY: 450});
+
+      //ReactDOM.unmountComponentAtNode(component);
+    })
+  });
+
+
+});
