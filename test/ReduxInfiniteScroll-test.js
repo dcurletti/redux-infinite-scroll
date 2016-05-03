1b0cffab53618ef49af10f4d9192ca83bb63bce6
Chris Conley <pair+chrisconley@realscout.com>
2016-05-03 10:05:41 -0700

    Adds basic scroll tests

--- test/ReduxInfiniteScroll-test.js
+++ test/ReduxInfiniteScroll-test.js
@@ -3,74 +3,123 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import ReduxInfiniteScroll from '../src/ReduxInfiniteScroll';
 
+class Shell extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: props.initialItems
+    }
+  }
+
+  _loadMore() {
+    var currentItems = this.state.items;
+    var newItems = this.props.additionalItems;
+
+    this.setState({
+      items: currentItems.concat(newItems)
+    });
+  }
+
+  _renderItems() {
+    console.log(this.state.items);
+    return this.state.items.map((item) => {
+      return (
+        <div style={{'height': this.props.itemHeight}}>{item}</div>
+      )
+    })
+  }
+
+  render () {
+    console.log(this);
+    return (
+      <div style={{overflow:'scroll'}}>
+        <ReduxInfiniteScroll containerHeight={this.props.containerHeight}
+                             loadMore={this._loadMore.bind(this)} >
+          {this._renderItems()}
+        </ReduxInfiniteScroll>
+      </div>
+    )
+  }
+}
+
 describe('ReduxInfiniteScroll', () => {
-  let loadMore, items;
 
   beforeEach(() => {
-    loadMore = () => {
-      items = items.concat(['item 4', 'item 5'])
-    };
-
-    items = [<div height="250px"></div>, <div height="250px"></div>, <div height="250px"></div>];
-  });
-
-  describe('default props', () => {
-    it('defaults to 100%', () => {
-      const reactElement = <ReduxInfiniteScroll loadMore={loadMore}
-                                             items={items}/>;
-      const render = TestUtils.renderIntoDocument(reactElement);
-      const node = ReactDOM.findDOMNode(render);
-      expect(node.style.height).toEqual('100%');
-    });
+    document.body.innerHTML = "";
   });
 
   describe('containerHeight', () => {
     it('defaults to 100%', () => {
-      const reactElement = <ReduxInfiniteScroll loadMore={loadMore}
-                                             items={items}/>;
+      const reactElement = <ReduxInfiniteScroll loadMore={function(){}}
+                                                items={[]}/>;
       const render = TestUtils.renderIntoDocument(reactElement);
       const node = ReactDOM.findDOMNode(render);
       expect(node.style.height).toEqual('100%');
     });
+
     it('should set the height of the div when given the correct props', () => {
       const reactElement = <ReduxInfiniteScroll containerHeight="500px"
-                                             loadMore={loadMore}
-                                             items={items}/>;
+                                                loadMore={function(){}}
+                                                items={[]}/>;
       const render = TestUtils.renderIntoDocument(reactElement);
       const node = ReactDOM.findDOMNode(render);
       expect(node.style.height).toEqual('500px');
     });
   });
 
-  xdescribe('threshold', () => {
-    it('defaults to setting the threshold to 100', () => {
-      const reactElement = <ReduxInfiniteScroll containerHeight="500px"
-                                             loadMore={loadMore}
-                                             items={items}/>;
-      const render = TestUtils.renderIntoDocument(reactElement);
-      const node = ReactDOM.findDOMNode(render);
-      //node.scrollTop = 499;
-      //reactElement.props.loadMore();
-      TestUtils.Simulate.scroll(node, {deltaY: 450});
+  describe('threshold default', () => {
+    it('should load more items at scroll of 101', (done) => {
+      const reactElement = <Shell
+        initialItems={['hi', 'hi', 'hi', 'hi']}
+        additionalItems={['bye', 'bye']}
+        itemHeight="100px"
+        containerHeight="200px"
+      />;
+      var component = ReactDOM.render(reactElement, document.body, function(){});
+      const shellNode = ReactDOM.findDOMNode(component);
+      var scrollNode = shellNode.children[0];
+      scrollNode.style["overflow"] = "scroll";
 
-      expect(items).toEqual('')
-    });
+      expect(scrollNode.children.length).toEqual(4)
 
-    it('actually adds the element to the page', () => {
-      const reactElement = <ReduxInfiniteScroll containerHeight="500px"
-                                             loadMore={loadMore}
-                                             items={items}/>;
-      //const render = TestUtils.renderIntoDocument(reactElement);
+      scrollNode.scrollTop = 101;
+      TestUtils.Simulate.scroll(scrollNode, {
+        target: scrollNode
+      });
 
+      setTimeout(function() {
+        expect(scrollNode.children.length).toEqual(6);
+        done();
+      }, 0);
 
-      var parentDiv = document.createElement('div');
-      document.body.appendChild(parentDiv);
-      var component = ReactDOM.render(reactElement, parentDiv);
-      const node = ReactDOM.findDOMNode(component);
-      TestUtils.Simulate.scroll(node, {deltaY: 450});
+    }, 100);
 
-      //ReactDOM.unmountComponentAtNode(component);
-    })
+    it('should not load more items at scroll of 100', (done) => {
+      const reactElement = <Shell
+        initialItems={['hi', 'hi', 'hi', 'hi']}
+        additionalItems={['bye', 'bye']}
+        itemHeight="100px"
+        containerHeight="200px"
+      />;
+      var component = ReactDOM.render(reactElement, document.body, function(){});
+      const shellNode = ReactDOM.findDOMNode(component);
+      var scrollNode = shellNode.children[0];
+      scrollNode.style["overflow"] = "scroll";
+
+      expect(scrollNode.children.length).toEqual(4)
+
+      scrollNode.scrollTop = 100;
+      TestUtils.Simulate.scroll(scrollNode, {
+        target: scrollNode
+      });
+
+      setTimeout(function() {
+        expect(scrollNode.children.length).toEqual(4);
+        done();
+      }, 0);
+
+    }, 100);
   });
 
 
